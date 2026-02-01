0c2580bdb5181d00956f1ded0e937d4190607caa
NYC <cnybb0415@naver.com>
2026-02-01 20:33:13 +0900

    Adjust KWANGYA icon position

--- src/components/QuickActionsBar.tsx
+++ src/components/QuickActionsBar.tsx
@@ -47,7 +47,7 @@ function IconMusic({ size = 18 }: { size?: number }) {
 const actionIcons: Record<string, React.ReactNode> = {
   "원클릭 스트리밍": <IconMusic />,
   "KWANGYA 119 신고하기": (
-    <img src="/images/icon/siren.png" alt="" className="h-4 w-4" />
+    <img src="/images/icon/siren.png" alt="" className="relative top-[-1.5px] h-4 w-4" />
   ),
   "REVERXE 앨범구매": <ShoppingBag size={18} />,
   "앨범 구매": <ShoppingBag size={18} />,
